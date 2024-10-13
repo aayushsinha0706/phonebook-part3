c4eac4a56ae373fc053ce0840d0002b7b311d471
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2024-10-13 17:24:11 +0530

    phonebook backend step-8

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+
+morgan.token("req-body", (req) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    }
+    return
+})
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body"))
+
 
 let persons = [
     { 
